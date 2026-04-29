4eee53ce56de2305f6fe8ad0a6666815d32a6a02
Sean Dickinson <90267290+sean-dickinson@users.noreply.github.com>
2026-04-28 20:04:48 -0400

    Feat: Add Auth + Schools CRUD (#14)
    * feat: generate authentication
    
    * feat: seed
    
    * feat: schools
    
    * feat: school seed
    
    * fix: update view
    
    * deps: install tailwind
    
    * feat: add daisyui
    
    * feat: style login
    
    * feat: style schools index
    
    * feat: style passwords
    
    * Feat: Students CRUD (#16)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+return unless Rails.env.development?
+
+User.find_or_create_by!(email_address: "admin@example.com") do |user|
+  user.name = "Admin"
+  user.password = "password"
+end
+
+school = School.find_or_create_by!(name: "Example School")
+
+def maybe(&block)
+  return unless Faker::Boolean.boolean
+
+  block.call
+end
+
+def build_student_attrs(overrides = {})
+  {
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: maybe { Faker::Internet.email(domain: 'example.com') },
+    gender: maybe { Student.genders.keys.sample }
+  }.merge(overrides)
+end
+
+# randomly sample 3 grade levels and create 10 students for each grade level
+grades = (1..12).to_a.sample(3)
+grades.each do |grade|
+  students = 10.times.map { build_student_attrs(grade_level: grade) }
+  school.students.create!(students)
+end
