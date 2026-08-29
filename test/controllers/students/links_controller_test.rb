6f740a105edeb17693dc2861e4b7350eef1cbfa8
Jonathan Knapp <jon@coffeeandcode.com>
2026-08-29 15:43:06 -0400

    Fix tests

--- test/controllers/students/links_controller_test.rb
+++ test/controllers/students/links_controller_test.rb
@@ -5,7 +5,7 @@ class Students::LinksControllerTest < ActionDispatch::IntegrationTest
     @student = students(:ada)
     student_sign_in_as @student
     @survey = links(:survey_one)
-    @game = links(:game_one)
+    @other_link = links(:survey_two)
   end
 
   test "unauthenticated request redirects to student login" do
@@ -20,10 +20,10 @@ class Students::LinksControllerTest < ActionDispatch::IntegrationTest
     assert_select "iframe[src='#{@survey.url}']"
   end
 
-  test "shows game page with iframe" do
-    get students_link_url(@game)
+  test "shows link page with iframe" do
+    get students_link_url(@other_link)
     assert_response :success
-    assert_select "iframe[src='#{@game.url}']"
+    assert_select "iframe[src='#{@other_link.url}']"
   end
 
   test "shows link type badge" do
@@ -32,7 +32,7 @@ class Students::LinksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "raises not found for link outside student classroom" do
-    other_link = links(:game_one)
+    other_link = links(:survey_two)
     student_sign_out
     student_sign_in_as students(:grace)  # grace is in classroom two, which has a different program
 
